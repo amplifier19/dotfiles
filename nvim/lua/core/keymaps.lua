79d9c3ef341527e18d07faf974834ed518c29abc
amplifier19 <dimitris.leontaridis@yahoo.gr>
2025-03-01 16:02:46 +0200

    Adding nvim dot files

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,13 @@
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
